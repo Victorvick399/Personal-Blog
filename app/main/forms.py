9d36df42147dcfbcec17622abac8fa4016d0b054
Victorvick399 <victorvick399@gmail.com>
2019-12-03 04:14:50 +0300

    create a post , update post,update profile and comment forms

---
+++ app/main/forms.py
@@ -0,0 +1,26 @@
+from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileAllowed
+from flask_login import current_user
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, TextAreaField , RadioField
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError , Required
+from app.models import User
+
+
+class PostForm(FlaskForm):
+    title = StringField('Title', validators=[DataRequired()])
+    content = TextAreaField('Content', validators=[DataRequired()])
+    submit = SubmitField('Post')
+
+class UpdatePostForm(FlaskForm):
+    title = StringField('Title', validators=[DataRequired()])
+    content = TextAreaField('Content', validators=[DataRequired()])
+    submit = SubmitField('Update Post')
+
+class UpdateProfile(FlaskForm):
+    bio = TextAreaField('Tell us about you.',validators = [Required()])
+    submit = SubmitField('Submit') 
+
+class CommentsForm(FlaskForm):
+    comment = TextAreaField('Comment', validators=[Required()])
+    vote=RadioField('default field arguments', choices=[('1', 'UpVote'), ('1', 'DownVote')])
+    submit = SubmitField('SUBMIT')  
